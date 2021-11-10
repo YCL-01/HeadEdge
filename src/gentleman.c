ef8277478497bd6f7f2734bf67c1cfbc677d11ce
youngchanlim <64450791+YCL-01@users.noreply.github.com>
2021-11-11 07:21:23 +0900

    Add files via upload

---
+++ src/gentleman.c
@@ -0,0 +1,38 @@
+#include "gentleman.h"
+
+void sigGenerator(sig_B genSig);
+
+void *counter(void *Args){
+    threadArgs_B *args = (threadArgs_B *)Args;
+    sig_A *counterSig = args->counterSig;
+    sig_B *newSigIn = args->newSigIn;
+    sig_B *newSigOut = args->newSigOut;
+    
+    for(int i=0; i<50; i++){
+        sprintf(newSigIn->statcode, "%s%s", counterSig->car_0, counterSig->ped_0);
+        sprintf(newSigOut->statcode, "%s%s", counterSig->car_1, counterSig->ped_1);
+        printf("newSigIn: %s\nnewSigOut: %s\n", newSigIn->statcode, newSigOut->statcode);
+        usleep(CNT_CYCLE);
+    }
+}
+
+void gentleman(int shmId_A, int shmId_B){
+    sig_A *counterSig;
+    sig_B newSigIn;
+    sig_B newSigOut;
+    threadArgs_B counterArgs;
+
+    void *shmAddr;
+    if((shmAddr = shmat(shmId_A, (void *)0, 0)) == (void *)-1) {
+        perror("Shmat failed");
+        return 1;
+    }
+
+    counterSig = (sig_A *)shmAddr;
+    pthread_t counterThread;
+    counterArgs.counterSig = counterSig;
+    counterArgs.newSigIn = &newSigIn;
+    counterArgs.newSigOut = &newSigOut;
+    pthread_create(&counterThread, NULL, counter, (void *)&counterArgs);
+    pthread_join(counterThread, NULL);   
+}
