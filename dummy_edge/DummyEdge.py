03bf729dce50a2b66b2fb19baf0121c489f126a9
youngchanlim <64450791+YCL-01@users.noreply.github.com>
2021-11-11 17:15:50 +0900

    Add files via upload

--- dummy_edge/DummyEdge.py
+++ dummy_edge/DummyEdge.py
@@ -5,40 +5,42 @@ import time
 from multiprocessing import Process
 
 def tcp_client_car(cam_id, serv_ip, serv_port):
+    #소켓 생성
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    #연결
+    print(serv_ip, serv_port)
+    sock.connect((serv_ip, serv_port)) 
+    
     while True:    
-        s = cam_id + " " + str(random.randint(0,4))
+        s = cam_id + " " + str(random.randint(0,99))
         print(s)
-        #소켓 생성
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        #연결
-        sock.connect((serv_ip, serv_port)) 
         #전송
         sock.send(s.encode())
         #100ms 쉼
-        time.sleep(0.01)
+        time.sleep(0.1)
 
 def tcp_client_ped(ped_id, serv_ip, serv_port):
-    while True:    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    #연결
+    result = sock.connect((serv_ip, serv_port))
+    print("result: ", result) 
+    while True:
         s = ped_id + " " + str(random.randint(0,4))
-        print(s)
-        #소켓 생성
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        #연결
-        sock.connect((serv_ip, serv_port)) 
+        print(s)    
         #전송
         sock.send(s.encode())
         #100ms 쉼
-        time.sleep(0.01)
+        time.sleep(0.1)
 
 if __name__ == '__main__':
 
-    serv_ip = "192.168.0.10"
+    serv_ip = '192.168.0.5'
     serv_port = 2555
     
     #프로세스 생성
     p1 = Process(target=tcp_client_car, args=('1', serv_ip, serv_port))
-    p2 = Process(target=tcp_client_car, args=('2', serv_ip, serv_port))
-    p3 = Process(target=tcp_client_ped, args=('3', serv_ip, serv_port))
+    p2 = Process(target=tcp_client_ped, args=('2', serv_ip, serv_port))
+    p3 = Process(target=tcp_client_car, args=('3', serv_ip, serv_port))
     p4 = Process(target=tcp_client_ped, args=('4', serv_ip, serv_port))
 
     #프로세스 시작
